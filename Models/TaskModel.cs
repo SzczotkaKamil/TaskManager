5e6fa4cd520701425d157a60fd641750f957577e
SzczotkaKamil <szkamil1999@wp.pl>
2021-07-13 22:01:26 +0200

    Add project files.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{[Table("Tasks")]
+    public class TaskModel
+    {
+        
+
+        [DisplayName("Id zadania")]
+        [Key]
+        public int TaskId { get; set; }
+        [DisplayName("Nazwa")]
+        [Required(ErrorMessage ="Pole Nazwa jest wymagane.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Szczegóły")]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        [DisplayName("Status")]
+        public bool Done { get; set; }
+    }
+}
